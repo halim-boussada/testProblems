f2a7f9af1e78066a189c5ee0bcc8f3909619c97e
halim-boussada <66827428+halim-boussada@users.noreply.github.com>
2022-02-25 02:58:27 +0100

    Update index.js

--- algorithm/index.js
+++ algorithm/index.js
@@ -1,8 +1,62 @@
+function replaceString(optionrule, indexStart, indexEnd) {
+  let replacementArr = [];
+  let theReplacement = optionrule.slice(indexStart + 1, indexEnd);
+  if (theReplacement.includes("AND")) {
+    theReplacement.split(" AND ").forEach((element) => {
+      replacementArr.push(JSON.parse(element));
+    });
+    let a = optionrule.replace(
+      optionrule.slice(indexStart, indexEnd + 1),
+      JSON.stringify({ and: replacementArr })
+    );
+    return a;
+  } else {
+    theReplacement.split(" OR ").forEach((element) => {
+      replacementArr.push(JSON.parse(element));
+    });
+    let a = optionrule.replace(
+      optionrule.slice(indexStart, indexEnd + 1),
+      JSON.stringify({ or: replacementArr })
+    );
+    return a;
+  }
+}
+function stringToObject(input) {
+  input = input.replace(/[{}]/g, "");
+  let lastClosingIndex;
+  let i = input.split("").length - 1;
+  while (input.includes("(")) {
+    if (input[i] == ")") {
+      lastClosingIndex = i;
+      i--;
+    } else if (input[i] == "(") {
+      input = replaceString(input, i, lastClosingIndex);
+      i = input.split("").length - 1;
+    } else {
+      i--;
+    }
+  }
+  let arr = [];
+  if (input.includes("AND")) {
+    input.split(" AND ").forEach((element) => {
+      arr.push(JSON.parse(element));
+    });
+    return {
+      and: arr,
+    };
+  } else {
+    input.split(" OR ").forEach((element) => {
+      arr.push(JSON.parse(element));
+    });
+    return {
+      or: arr,
+    };
+  }
+}
+
 const optionRule =
   "{1069} AND ({1070} OR {1071} OR {1072}) AND {1244} AND ({1245} OR {1339})";
 
-const output = {
-  and: [1069, { or: [1070, 1071, 1072] }, 1244, { or: [1245, 1339] }],
-};
+let result = stringToObject(optionRule);
 
-// Output Example
+console.log("result:", result);
